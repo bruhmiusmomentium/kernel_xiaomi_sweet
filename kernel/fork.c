a28ca0765b2a705bdb524e708caee530e40eec8f
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-01-29 05:23:27 -0500

    kernel: fork: also boost LLCCBW
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2230,8 +2230,9 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 50 ms when userspace launches an app */
+	/* Boost CPU & DDR to the max for 50 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
