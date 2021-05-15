c2d9e9f88f299804234eaaa1b0787b444c0e44f8
CloudedQuartz <ravenklawasd@gmail.com>
2021-05-15 10:39:03 +0530

    Revert "kernel: fork: remove input boosting"
    This reverts commit 23ac9230a63a5280c2200cb2aadea7441af7ea76.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,6 +94,7 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -2232,6 +2233,7 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU & DDR to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
