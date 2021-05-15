365ac746d92e4d08f98df59542f16340e80a6c32
CloudedQuartz <ravenklawasd@gmail.com>
2021-05-15 10:20:35 +0530

    kernel: fork: remove input boosting
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,7 +94,6 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -2233,7 +2232,6 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
